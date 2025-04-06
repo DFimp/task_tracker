a7eaf63bba135a3017402555aa50aa467258a7c6
DFimp <ilia.fimp@gmail.com>
2025-04-07 00:26:09 +0300

    realize route report

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,24 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useAuth } from '../../context/AuthContext';
 import styles from './Header.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const { logout } = useAuth();
+  const navigate = useNavigate();
 
   return (
     <header className={styles.header}>
-      <button onClick={logout} className={styles.logoutButton}>
-        Выйти
-      </button>
+      <div className={styles.center}>
+        <button onClick={() => navigate('/report')} className={styles.reportButton}>
+          Отчёт
+        </button>
+      </div>
+      <div className={styles.right}>
+        <button onClick={logout} className={styles.logoutButton}>
+          Выйти
+        </button>
+      </div>
     </header>
   );
 };
